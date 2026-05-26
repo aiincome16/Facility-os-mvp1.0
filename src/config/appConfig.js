f237ebdd910ffcb8331e036a007c1db243956448
aiincome16 <marketing.aiincomelab@gmail.com>
2026-05-27 01:57:38 +0200

    Update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,1 +1,23 @@
+export const APP_CONFIG = {
+  APP_NAME: "Facility-OS",
+  VERSION: "MVP-1",
 
+  // Entwicklungsmodus
+  TEST_MODE: true,
+
+  // Sicherheit
+  SESSION_TIMEOUT_MINUTES: 480,
+  PASSWORD_MODE: "DEVELOPMENT",
+
+  // Features
+  QR_REQUIRED: true,
+  GPS_REQUIRED: true,
+  OFFLINE_MODE: true,
+
+  // Automatisierung
+  AUTO_REPLACEMENT_SEARCH: true,
+  CONTACT_PERMISSION_REQUIRED: true,
+
+  // Nachvollziehbarkeit
+  AUDIT_LOGGING: true
+};
