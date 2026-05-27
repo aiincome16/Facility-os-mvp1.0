494ddd07afdc8df755f6e3b774b61f4fba25038c
aiincome16 <marketing.aiincomelab@gmail.com>
2026-05-27 02:01:59 +0200

    Update idGenerator.js

--- src/backend/utils/idGenerator.js
+++ src/backend/utils/idGenerator.js
@@ -1,1 +1,23 @@
+const counters = {};
 
+export function generateId(prefix) {
+  if (!prefix) {
+    throw new Error("Prefix fehlt");
+  }
+
+  if (!counters[prefix]) {
+    counters[prefix] = 1;
+  }
+
+  const id = `${prefix}-${String(counters[prefix]).padStart(3, "0")}`;
+
+  counters[prefix]++;
+
+  return id;
+}
+
+export function resetIdCounter(prefix) {
+  if (prefix) {
+    counters[prefix] = 1;
+  }
+}
