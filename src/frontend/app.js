8a1ee98a40ee2544197233a180925df3d5a6f7b4
aiincome16 <marketing.aiincomelab@gmail.com>
2026-06-07 11:43:20 +0200

    app.js Test Notifikation

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -5,12 +5,13 @@ import { bindQrEvents } from "./qrUi.js";
 import { bindShiftEvents } from "./shiftUi.js";
 import { bindObjectEvents } from "./objectUi.js";
 import { bindRoomEvents } from "./roomUi.js";
+import { bindNotificationEvents } from "./notificationUi.js";
 
 document.getElementById("app").innerHTML = `
   <div class="app-shell">
     <div class="header-card">
       <h1>Facility-OS</h1>
-      <p>Test Room Import</p>
+      <p>Test Notification Import</p>
     </div>
 
     <div class="section-card">
@@ -23,7 +24,7 @@ bindModalEvents();
 bindQrEvents();
 bindShiftEvents();
 bindObjectEvents();
-bindRoomEvents();
+bindRoomEvents();bindNotificationEvents();
 
 showToast(
   "Room Import funktioniert",
