7c84de4a0454756b793d181ad7e9da0b369bc0b7
aiincome16 <marketing.aiincomelab@gmail.com>
2026-06-07 09:55:56 +0200

    app.js Test rooms

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -2,22 +2,19 @@ import { renderDashboard } from "./dashboard.js";
 import { bindModalEvents } from "./modalUi.js";
 import { showToast } from "./toastUi.js";
 import { bindQrEvents } from "./qrUi.js";
-
-import {
-  bindShiftEvents
-} from "./shiftUi.js";
-
+import { bindShiftEvents } from "./shiftUi.js";
 import { bindObjectEvents } from "./objectUi.js";
+import { bindRoomEvents } from "./roomUi.js";
 
 document.getElementById("app").innerHTML = `
   <div class="app-shell">
     <div class="header-card">
       <h1>Facility-OS</h1>
-      <p>Test Object Import</p>
+      <p>Test Room Import</p>
     </div>
 
     <div class="section-card">
-      objectUi.js wird geladen.
+      roomUi.js wird geladen.
     </div>
   </div>
 `;
@@ -26,8 +23,9 @@ bindModalEvents();
 bindQrEvents();
 bindShiftEvents();
 bindObjectEvents();
+bindRoomEvents();
 
 showToast(
-  "Object Import funktioniert",
+  "Room Import funktioniert",
   "SUCCESS"
 );
