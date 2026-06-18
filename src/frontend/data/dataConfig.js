62daa62b5483eb08621c6c0c9744bcb9cd4d09dd
aiincome16 <marketing.aiincomelab@gmail.com>
2026-06-18 11:07:53 +0200

    Create dataConfig.js

---
+++ src/frontend/data/dataConfig.js
@@ -0,0 +1,11 @@
+export const DATA_CONFIG = {
+  users: "./data/users.json",
+  objects: "./data/objects.json",
+  rooms: "./data/rooms.json",
+  tasks: "./data/tasks.json",
+  materials: "./data/materials.json",
+  shifts: "./data/shifts.json",
+  qrCodes: "./data/qrCodes.json",
+  notifications: "./data/notifications.json",
+  customerRequests: "./data/customerRequests.json"
+};
